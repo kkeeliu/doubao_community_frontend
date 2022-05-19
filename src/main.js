7764941def797d0dedece466491bb8436ff62146
liuke <liukechn@163.com>
2022-05-19 19:45:11 +0800

    全局导入ELE

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//导入Elementui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//导入Buef
+import Buefy from  'buefy'
+import 'buefy/dist/buefy.css'
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(Buefy)
 
 new Vue({
   router,
