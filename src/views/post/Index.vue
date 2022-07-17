a693fe6ae5e2f46c92ac82d4498bd472fb7eb6e7
liuke <liukechn@163.com>
2022-07-17 20:25:04 +0800

    帖子列表用el-card实现

--- src/views/post/Index.vue
+++ src/views/post/Index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <el-card>
-      <div class="Header">
+      <div slot="header">
           帖子列表
       </div>
       <div>
-        
+        body
       </div>
     </el-card>
   </div>
