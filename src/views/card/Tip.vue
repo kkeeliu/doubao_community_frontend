fcfaa75c1a0d2fb84f9149de933298dadd128db4
liuke <liukechn@163.com>
2022-07-17 22:16:58 +0800

    每日一句功能实现

--- src/views/card/Tip.vue
+++ src/views/card/Tip.vue
@@ -1,29 +1,44 @@
 <template>
   <div>
     <el-card>
-      <div slot="header">
-         🥳 每日一句
-      </div> 
+      <div slot="header">🥳 每日一句</div>
       <div>
         <!-- 文字居右 块级元素 -->
-        <div class="has-text-left block">
-            条条大路通罗马
-        </div>
+        <div class="has-text-left block">{{tip.content}}</div>
         <!-- margin-top:5px -->
-        <div class="has-text-right mt-5 block">
-            ——托尔斯泰
-        </div>
+        <div class="has-text-right mt-5 block">——{{tip.author}}</div>
       </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import { getRequest } from '../../utills/api';
 export default {
-    name: "Tip"
-}
+  name: "Tip",
+  data() {
+    return {
+      tip: {
+        content: "",
+        author: "",
+      },
+    };
+  },
+ mounted() {
+  this.getTipToday();
+ },
+  methods: {
+    getTipToday(){
+      getRequest('/tip/today').then((resp)=>{
+        if(resp){
+          this.tip.content = resp.data.content;
+          this.tip.author = resp.data.author;
+        }
+      })
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
