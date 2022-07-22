4c39588a736314493ba3fa76208b7ce0f36c08e6
liuke <liukechn@163.com>
2022-07-22 15:12:59 +0800

    推广功能实现

--- src/views/card/Promotion.vue
+++ src/views/card/Promotion.vue
@@ -5,15 +5,35 @@
          🥂 推广
       </div>
       <div>
-        body
+        <p v-for="(item,index) in list"  :key=index class="block">
+        <a :href="item.link" target="_blank">{{item.title}}</a>
+        </p>
       </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import { getRequest } from '../../utills/api'
 export default {
-  name: "Promotion"
+  name: "Promotion",
+  data(){
+    return {
+      list:[]
+    }
+  },
+  mounted(){
+    this.initData()
+  },
+  methods:{
+    initData(){
+      getRequest('/promotion/list').then((resp)=>{
+        if(resp){
+          this.list = resp.data
+        }
+      })
+    }
+  }
 }
 </script>
 
