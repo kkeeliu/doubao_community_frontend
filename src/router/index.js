34b53b886ea12b9cf780ba16bfc12a06cf99a620
liuke <liukechn@163.com>
2022-07-22 16:12:23 +0800

    集成注册功能

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,18 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: '首页',
     component: () => import('@/views/Home')
   },
   {
     path: '/404',
-    name: '404',
+    name: 404,
     component: () => import('@/views/common/404')
+  },
+  {
+    path: '/register',
+    name: '注册',
+    component: () => import('@/views/common/register')
   }
 ]
 
