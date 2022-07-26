5319c72c577728759aae6144b2aa3f88d4d2b0b4
liuke <liukechn@163.com>
2022-07-26 10:13:35 +0800

    进度条 日夜模式等

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
+
+  <div>
+     <div class="mb-5">
+      <Header></Header>
+  </div>
+
+
   <!-- container 容器类 居中显示-->
   <div class="container">
     <router-view />
   </div>
+  </div>
+ 
 </template>
-
+<script>
+import Header from "@/components/layout/Header";
+export default {
+  name: "App",
+  components: { Header},
+};
+</script>
 <style>
 </style>
--- src/main.js
+++ src/main.js
@@ -10,11 +10,40 @@ import Buefy from  'buefy'
 import 'buefy/dist/buefy.css'
 //导入全局css样式
 import '@/assets/app.css'
-
+import {deleteRequest, getRequest, postRequest, putRequest} from "./utills/api";
+import NProgress from "nprogress"
+import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.use(Buefy)
+
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  if (window.sessionStorage.getItem('tokenStr')){
+    //判断用户信息是否存在，如果不存在
+    if (!window.sessionStorage.getItem('user')){
+      return getRequest('/user/info').then(resp=>{
+        if (resp){
+          //存入用户信息
+          window.sessionStorage.setItem('user',JSON.stringify(resp));
+          next();
+        }
+      })
+    }
+    next();
+    NProgress.done()
+  } else {
+    if (to.path=='/'){
+      next();
+      NProgress.done()
+    }else{
+      next('/?redirect='+to.path);
+      NProgress.done()
+    }
+  }
+})
 
 new Vue({
   router,
